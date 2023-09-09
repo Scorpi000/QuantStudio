26cc00bb563da5105e444c631a7f4df796869734
scorpi000 <8178725+scorpi000@user.noreply.gitee.com>
2023-09-09 09:36:18 +0000

    MemoryDB multiprocess
    Signed-off-by: scorpi000 <8178725+scorpi000@user.noreply.gitee.com>

--- QuantStudio/FactorDataBase/MemoryDB.py
+++ QuantStudio/FactorDataBase/MemoryDB.py
@@ -24,55 +24,61 @@ class _FactorTable(FactorTable):
         TargetDT = Either(None, Date, arg_type="DateTime", label="目标时点", order=4)
     
     def __init__(self, name, fdb, sys_args={}, **kwargs):
-        self._Data = fdb._Data[name]
-        self._TableMeta = fdb._TableMeta.get(name, {})
-        self._FactorMeta = fdb._FactorMeta.get(name, {})
+        self._Lock = fdb._Lock
+        self._Data = fdb._Data
+        self._TableMeta = fdb._TableMeta
+        self._FactorMeta = fdb._FactorMeta
         return super().__init__(name=name, fdb=fdb, sys_args=sys_args, **kwargs)
     
     @property
     def FactorNames(self):
-        return sorted(self._Data.keys())
+        return sorted(self._Data[self._Name].keys())
     
     def getMetaData(self, key=None, args={}):
-        if not self._TableMeta:
-            return (pd.Series() if key is None else None)
-        if key is None:
-            return pd.Series(self._TableMeta)
-        else:
-            return self._TableMeta.get(key, None)
+        with self._Lock.
+            if self._Name not in self._TableMeta:
+                return (pd.Series() if key is None else None)
+            if key is None:
+                return pd.Series(self._TableMeta[self._Name])
+            else:
+                return self._TableMeta[self._Name].get(key, None)
     
     def getFactorMetaData(self, factor_names=None, key=None, args={}):
         AllFactorNames = self.FactorNames
         if factor_names is None: factor_names = AllFactorNames
         elif set(factor_names).isdisjoint(AllFactorNames): return super().getFactorMetaData(factor_names=factor_names, key=key, args=args)
         MetaData = {}
-        for iFactorName in factor_names:
-            if iFactorName in self._FactorMeta:
-                if key is None:
-                    MetaData[iFactorName] = self._FactorMeta[iFactorName]
-                else:
-                    MetaData[iFactorName] = self._FactorMeta[iFactorName].get(key, None)
-        if not MetaData: return super().getFactorMetaData(factor_names=factor_names, key=key, args=args)
-        if key is None: return pd.DataFrame(MetaData).T.reindex(index=factor_names)
-        else: return pd.Series(MetaData).reindex(index=factor_names)
+        with self._Lock:
+            FactorMeta = self._FactorMeta.get(self._Name, {})
+            for iFactorName in factor_names:
+                if iFactorName in FactorMeta:
+                    if key is None:
+                        MetaData[iFactorName] = FactorMeta.get(iFactorName, {})
+                    else:
+                        MetaData[iFactorName] = FactorMeta.get(iFactorName, {}).get(key, None)
+            if not MetaData: return super().getFactorMetaData(factor_names=factor_names, key=key, args=args)
+            if key is None: return pd.DataFrame(MetaData).T.reindex(index=factor_names)
+            else: return pd.Series(MetaData).reindex(index=factor_names)
     
     def getID(self, ifactor_name=None, idt=None, args={}):
-        if ifactor_name is None: ifactor_name = self.FactorNames[0]
-        if idt is None:
-            return sorted(self._Data[ifactor_name].columns)
-        elif idt not in self._Data[ifactor_name].index:
-            return []
-        else:
-            return sorted(self._Data[ifactor_name].loc[idt].dropna().index)
+        with self._Lock:
+            if ifactor_name is None: ifactor_name = self.FactorNames[0]
+            if idt is None:
+                return sorted(self._Data[self._Name][ifactor_name].columns)
+            elif idt not in self._Data[self._Name][ifactor_name].index:
+                return []
+            else:
+                return sorted(self._Data[self._Name][ifactor_name].loc[idt].dropna().index)
     
     def getDateTime(self, ifactor_name=None, iid=None, start_dt=None, end_dt=None, args={}):
-        if ifactor_name is None: ifactor_name = self.FactorNames[0]
-        if iid is None:
-            DTs = self._Data[ifactor_name].index
-        elif iid not in self._Data[ifactor_name].columns:
-            return []
-        else:
-            DTs = self._Data[ifactor_name].loc[:, iid].dropna().index
+        with self._Lock:
+            if ifactor_name is None: ifactor_name = self.FactorNames[0]
+            if iid is None:
+                DTs = self._Data[self._Name][ifactor_name].index
+            elif iid not in self._Data[self._Name][ifactor_name].columns:
+                return []
+            else:
+                DTs = self._Data[self._Name][ifactor_name].loc[:, iid].dropna().index
         if start_dt: DTs = DTs[DTs>=start_dt]
         if end_dt: DTs = DTs[DTs<=end_dt]
         return sorted(DTs)
@@ -82,7 +88,8 @@ class _FactorTable(FactorTable):
         return Panel(Data, items=factor_names, major_axis=dts, minor_axis=ids)
     
     def _readFactorData(self, ifactor_name, ids, dts, args={}):
-        return self._Data[ifactor_name].reindex(index=dts, columns=ids)
+        with self._Lock:
+            return self._Data[self._Name][ifactor_name].reindex(index=dts, columns=ids)
     
     def readFactorData(self, ifactor_name, ids, dts, args={}):
         TargetDT = args.get("目标时点", self._QSArgs.TargetDT)
@@ -160,34 +167,39 @@ class MemoryDB(WritableFactorDB):
     
     def renameTable(self, old_table_name, new_table_name):
         if old_table_name==new_table_name: return 0
-        if old_table_name not in self._Data: raise __QS_Error__("MemoryDB.renameTable: 表: '%s' 不存在!" % old_table_name)
-        if new_table_name in self._Data: raise __QS_Error__("MemoryDB.renameTable: 表 '"+new_table_name+"' 已存在!")
-        self._Data[new_table_name] = self._Data.pop(old_table_name)
-        if old_table_name in self._TableMeta:
-            self._TableMeta[new_table_name] = self._TableMeta.pop(old_table_name)
-        if old_table_name in self._FactorMeta:
-            self._FactorMeta[new_table_name] = self._FactorMeta.pop(old_table_name)
+        with self._Lock:
+            if old_table_name not in self._Data: raise __QS_Error__("MemoryDB.renameTable: 表: '%s' 不存在!" % old_table_name)
+            if new_table_name in self._Data: raise __QS_Error__("MemoryDB.renameTable: 表 '"+new_table_name+"' 已存在!")
+            self._Data[new_table_name] = self._Data.pop(old_table_name)
+            if old_table_name in self._TableMeta:
+                self._TableMeta[new_table_name] = self._TableMeta.pop(old_table_name)
+            if old_table_name in self._FactorMeta:
+                self._FactorMeta[new_table_name] = self._FactorMeta.pop(old_table_name)
         return 0
     
     def deleteTable(self, table_name):
-        self._Data.pop(table_name, None)
-        self._TableMeta.pop(table_name, None)
-        self._FactorMeta.pop(table_name, None)
+        with self._Lock:
+            self._Data.pop(table_name, None)
+            self._TableMeta.pop(table_name, None)
+            self._FactorMeta.pop(table_name, None)
         return 0
     
     def setTableMetaData(self, table_name, key=None, value=None, meta_data=None):
-        if meta_data is not None:
-            meta_data = dict(meta_data)
-        else:
-            meta_data = {}
-        if key is not None:
-            meta_data[key] = value
-        if table_name not in self._Data:
-            raise __QS_Error__("MemoryDB.getTable: 表 '%s' 不存在!" % table_name)
-        if table_name not in self._TableMeta:
-            self._TableMeta[table_name] = meta_data
-        else:
-            self._TableMeta[table_name].update(meta_data)
+        with self._Lock:
+            if meta_data is not None:
+                meta_data = dict(meta_data)
+            else:
+                meta_data = {}
+            if key is not None:
+                meta_data[key] = value
+            if table_name not in self._Data:
+                raise __QS_Error__("MemoryDB.getTable: 表 '%s' 不存在!" % table_name)
+            if table_name not in self._TableMeta:
+                self._TableMeta[table_name] = meta_data
+            else:
+                iDict = self._TableMeta[table_name]
+                iDict.update(meta_data)
+                self._TableMeta[table_name] = iDict
         return 0
     
     def setFactorDef(self, table_name, def_file, if_exists="update"):
@@ -210,32 +222,48 @@ class MemoryDB(WritableFactorDB):
     # ----------------------------因子操作---------------------------------
     def renameFactor(self, table_name, old_factor_name, new_factor_name):
         if old_factor_name==new_factor_name: return 0
-        if table_name not in self._Data: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 不存在!" % (table_name, ))
-        if old_factor_name not in self._Data[table_name]: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 中不存在因子 '%s'!" % (table_name, old_factor_name))
-        if new_factor_name not in self._Data[table_name]: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 中的因子 '%s' 已存在!" % (table_name, new_factor_name))
-        self._Data[table_name][new_factor_name] = self._Data[table_name].pop(old_factor_name)
-        if (table_name in self._FactorMeta) and (old_factor_name in self._FactorMeta[table_name]):
-            self._FactorMeta[table_name][new_factor_name] = self._FactorMeta[table_name].pop(old_factor_name)
+        with self._Lock:
+            if table_name not in self._Data: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 不存在!" % (table_name, ))
+            if old_factor_name not in self._Data[table_name]: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 中不存在因子 '%s'!" % (table_name, old_factor_name))
+            if new_factor_name not in self._Data[table_name]: raise __QS_Error__("MemoryDB.renameFactor: 表 '%s' 中的因子 '%s' 已存在!" % (table_name, new_factor_name))
+            iDict = self._Data[table_name]
+            iDict[new_factor_name] = iDict.pop(old_factor_name)
+            self._Data[table_name] = iDict
+            if (table_name in self._FactorMeta) and (old_factor_name in self._FactorMeta[table_name]):
+                iDict = self._FactorMeta[table_name]
+                iDict[new_factor_name] = iDict.pop(old_factor_name)
+                self._FactorMeta[table_name] = iDict
         return 0
     
     def deleteFactor(self, table_name, factor_names):
         if table_name not in self._Data: return 0
-        FactorNames = set(self._Data[table_name])
-        if FactorNames.issubset(set(factor_names)):
-            return self.deleteTable(table_name)
-        for iFactorName in factor_names:
-            self._Data[table_name].pop(iFactorName)
-            if table_name in self._FactorMeta:
-                self._FactorMeta[table_name].pop(iFactorName)
+        with self._Lock:
+            FactorNames = set(self._Data[table_name])
+            if FactorNames.issubset(set(factor_names)):
+                self._Data.pop(table_name, None)
+                self._TableMeta.pop(table_name, None)
+                self._FactorMeta.pop(table_name, None)
+            else:
+                for iFactorName in factor_names:
+                    iDict = self._Data[table_name]
+                    iDict.pop(iFactorName, None)
+                    self._Data[table_name] = iDict
+                    if table_name in self._FactorMeta:
+                        iDict = self._FactorMeta[table_name]
+                        iDict.pop(iFactorName, None)
+                        self._FactorMeta[table_name] = iDict
         return 0
     
     def setFactorMetaData(self, table_name, ifactor_name, key=None, value=None, meta_data=None):
-        iMeta = self._FactorMeta.setdefault(table_name, {}).setdefault(ifactor_name, {})
-        if key is not None:
-            iMeta[key] = value
-        if meta_data is not None:
-            for iKey in meta_data:
-                iMeta[iKey] = meta_data[iKey]
+        with self._Lock:
+            Meta = self._FactorMeta.get(table_name, {})
+            iMeta = Meta.setdefault(ifactor_name, {})
+            if key is not None:
+                iMeta[key] = value
+            if meta_data is not None:
+                for iKey in meta_data:
+                    iMeta[iKey] = meta_data[iKey]
+            self._FactorMeta[table_name] = Meta
         return 0
     
     def writeFactorData(self, factor_data, table_name, ifactor_name, if_exists="update", data_type=None, **kwargs):
